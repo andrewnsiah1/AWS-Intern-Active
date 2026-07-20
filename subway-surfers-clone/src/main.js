d4b1c524106790fe9e70787d42be32bfd415a51f
Andrew Nsiah <aknsiah@amazon.com>
2026-07-20 15:16:50 -0700

    tweak: Lower max speed to 0.6, slightly slower acceleration

--- subway-surfers-clone/src/main.js
+++ subway-surfers-clone/src/main.js
@@ -23,8 +23,8 @@ let gameState = 'start'; // 'start', 'playing', 'over'
 let score = 0;
 let coins = 0;
 let speed = 0.255;
-const MAX_SPEED = 0.9;
-const SPEED_INCREMENT = 0.00005;
+const MAX_SPEED = 0.6;
+const SPEED_INCREMENT = 0.00004;
 
 // Scene setup
 const scene = new THREE.Scene();
